da6a5c135f9a5fb18d74d7f4df6f641c5c219a47
Matias Donnet <donnet.matias90@gmail.com>
2022-07-20 20:30:45 -0300

    Primer Practico

---
+++ Script_Practico_1.js
@@ -0,0 +1,17 @@
+var contador = 0;
+
+while (contador != 5) {
+    if ( contador == 0) {
+        console.log(" Estamos por empezar a contar");
+    }
+        
+        else if (contador == 1 || contador == 3) {
+        console.log("Iteracion insignificante, no nos gustan los numero impar");
+    }
+    else {
+        console.log("Iteracion Numero :" + contador);
+    }
+    contador = contador + 1;
+    
+}
+// JavaScript source code
